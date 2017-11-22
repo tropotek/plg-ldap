102e575ad10d528790375d773d0fd5c526a52392
mifsudm <mifsudm@unimelb.edu.au>
2017-11-23 10:07:48 +1100

    Minor Code Updates - 252s-dev

--- Ldap/Controller/InstitutionSettings.php
+++ Ldap/Controller/InstitutionSettings.php
@@ -135,7 +135,7 @@ class InstitutionSettings extends Iface
         $template = parent::show();
         
         // Render the form
-        $template->insertTemplate($this->form->getId(), $this->form->getRenderer()->show()->getTemplate());
+        $template->insertTemplate($this->form->getId(), $this->form->getRenderer()->show());
 
         return $template;
     }
