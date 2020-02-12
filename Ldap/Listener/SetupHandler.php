fc6b9ec8d9fff77eb3971b72ba1f7720f27f8843
mifsudm <mifsudm@unimelb.edu.au>
2020-02-13 10:49:54 +1100

    ~Auto: Commit

--- Ldap/Listener/SetupHandler.php
+++ Ldap/Listener/SetupHandler.php
@@ -26,9 +26,9 @@ class SetupHandler implements Subscriber
             if ($event->getRequest()->has('instHash')) {
                 $institution = $config->getInstitutionMapper()->findByHash($event->getRequest()->get('instHash'));
             }
-            if ($event->getRequest()->getAttribute('institutionId')) {
+            if ($event->getRequest()->attribute->get('institutionId')) {
                 /** @var \Uni\Db\Institution $inst */
-                $institution = $config->getInstitutionMapper()->find($event->getRequest()->getAttribute('institutionId'));
+                $institution = $config->getInstitutionMapper()->find($event->getRequest()->attribute->get('institutionId'));
             }
             $config->set('institution', $institution);
         }
